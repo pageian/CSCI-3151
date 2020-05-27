e9bcf35149fc02ee2e1fcbe903f612fc5036a80d
Ian Page <ian.page@dal.ca>
2020-05-27 10:03:00 -0300

    made more changes

--- Assignments/A1/main.py
+++ Assignments/A1/main.py
@@ -3,28 +3,29 @@ import pandas as pd
 import numpy as np
 
 def LRGradDesc(data, target, init_x1=0, init_x2=0, init_b=0,
-    learning_rate_w=0.0001, learning_rate_b=0.0001, max_iter=1000):
+    learning_rate_w=0.0001, learning_rate_b=0.0001, max_iter=100):
     
-    print(data)
-    print(target)
+    #print(data)
+    #print(target)
     N = len(data.iloc[:, 0])
     current_x1 = init_x1
     current_x2 = init_x2
     current_b = init_b
     for i in range(max_iter):
-        prediction = data[0] * init_x1 + data[1] * init_x2 + init_b
+        prediction = np.dot(data.iloc[:,0], current_x1) + np.dot(data.iloc[:,1], current_x2) + current_b
+        #print(target)
         cost = np.sum((prediction - target) ** 2) / (2. * N)
 
-        #x1_grad = -(2/N) * np.sum(data[0] * (prediction - target))
-        #x2_grad = -(2/N) * np.sum(data[1] * (prediction - target))
-        #b_grad = -(2/N) * np.sum(prediction - target)
+        x1_grad = np.dot((learning_rate_w/N), np.sum(np.dot(data.iloc[:,0], (prediction - target))))
+        x2_grad = np.dot((learning_rate_w/N), np.sum(np.dot(data.iloc[:,1], (prediction - target))))
+        b_grad = np.dot((learning_rate_b/N), np.sum(prediction - target))
+        print(x1_grad)
+    
+        current_x1 = current_x1 - x1_grad
+        current_x2 = current_x2 - x2_grad
+        current_b = current_b - b_grad
 
-        #current_x1 = current_x1 - (learning_rate_w * x1_grad)
-        #current_x2 = current_x2 - (learning_rate_w * x2_grad)
-        #current_b = current_b - (learning_rate_b * b_grad)
-
-        #print("[Iterantion " + i + "] x1: " + current_x1 + " x2: " + current_x2 + 
-        #    " b: " + current_b + " cost: " + cost)
+        #print("[Iteration " + str(i) + "] x1: " + str(current_x1) + " x2: " + str(current_x2) + " b: " + str(current_b) + " cost: " + str(cost))
 
     return current_x1, current_x2, current_b
 
