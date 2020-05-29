5a97e72795f27741b7657da9fcb4dbfe8e212fb1
Ian Page <ian.page@dal.ca>
2020-05-29 12:57:52 -0300

    Added loop over mulltiple degrees for part3

--- Assignments/A1/part3.py
+++ Assignments/A1/part3.py
@@ -3,45 +3,100 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error, r2_score
+
+# list of columns that require pre-processing
+string_val_cols = [
+    'MSZoning',
+    'Street',
+    'Alley',
+    'LotShape',
+    'LandContour',
+    'Utilities',
+    'LotConfig',
+    'LandSlope',
+    'Neighborhood',
+    'Condition1',
+    'Condition2',
+    'BldgType',
+    'HouseStyle',
+    'RoofStyle',
+    'RoofMatl',
+    'Exterior1st',
+    'Exterior2nd',
+    'MasVnrType',
+    'ExterQual',
+    'ExterCond',
+    'Foundation',
+    'BsmtQual',
+    'BsmtCond',
+    'BsmtExposure',
+    'BsmtFinType1',
+    'BsmtFinType2',
+    'Heating',
+    'HeatingQC',
+    'CentralAir',
+    'Electrical',
+    'KitchenQual',
+    'Functional',
+    'FireplaceQu',
+    'GarageType',
+    'GarageFinish',
+    'GarageQual',
+    'GarageCond',
+    'PavedDrive',
+    'PoolQC',
+    'Fence',
+    'MiscFeature',
+    'SaleType',
+    'SaleCondition']
+
+# create the Labelencoder object
+le = preprocessing.LabelEncoder()
 
 # Importing the dataset
 dataset = pd.read_csv("data-sets/housing-prices-dataset/train.csv")
-X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2].values
+X = dataset.iloc[:,1:len(dataset.loc[0,:])-1]
+y = dataset.iloc[:, len(dataset.loc[0,:])-1]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+print("processing data")
+for col in string_val_cols:
+    #print("Processing " + col)
+    X[col] = le.fit_transform(X[col].astype(str))
+X = np.nan_to_num(X)
+y = np.nan_to_num(y)
 
-# Fitting Linear Regression to the dataset
-lin_reg = LinearRegression()
-lin_reg.fit(X, y)
+for i in range(1,5):
+    print("### Degree = " + str(i) + " ###")
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# def viz_linear():
-#     plt.scatter(X, y, color='red')
-#     plt.plot(X, lin_reg.predict(X), color='blue')
-#     plt.title('Truth or Bluff (Linear Regression)')
-#     plt.xlabel('Position level')
-#     plt.ylabel('Salary')
-#     plt.show()
-#     return
-# viz_linear()
+    # Visualizing the Linear Regression results
+    poly_reg = PolynomialFeatures(degree=i)
+    print("Fitting model to polyreg")
+    X_poly = poly_reg.fit_transform(X_train)
 
-# Visualizing the Linear Regression results
-poly_reg = PolynomialFeatures(degree=4)
-X_poly = poly_reg.fit_transform(X)
-pol_reg = LinearRegression()
-pol_reg.fit(X_poly, y)
+    pol_reg = LinearRegression()
+    print("Fitting Model to linreg")
+    pol_reg.fit(X_poly, y_train)
 
-# Visualizing the Polymonial Regression results
-def viz_polymonial():
-    plt.scatter(X, y, color='red')
-    plt.plot(X, pol_reg.predict(poly_reg.fit_transform(X)), color='blue')
-    plt.title('Truth or Bluff (Linear Regression)')
-    plt.xlabel('Position level')
-    plt.ylabel('Salary')
-    plt.show()
-    return
-viz_polymonial()
+    print("Making prediction")
+    prediction = pol_reg.predict(X_poly)
 
-print(lin_reg.predict([[5.5]]))
-print(pol_reg.predict(poly_reg.fit_transform([[5.5]])))
+    rmse = np.sqrt(mean_squared_error(y_train,prediction))
+    r2 = r2_score(y_train,prediction)
+    print(rmse)
+    print(r2)
+
+    # Visualizing the Polymonial Regression results
+    def viz_polymonial():
+        plt.scatter(range(len(X_test)), y_test, color='red')
+        plt.plot(range(len(X_test)), pol_reg.predict(poly_reg.fit_transform(X_test)), color='blue')
+        title = 'House Sale Price Estimation (Degree = ' + str(i) + ')'
+        plt.title(title)
+        plt.xlabel('House')
+        plt.ylabel('Sale Price')
+        plt.show()
+        return
+    viz_polymonial()
