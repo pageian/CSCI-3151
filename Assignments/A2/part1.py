c8173525d7321b8cade995e3a4b1b6e084165719
Ian Page <ian.am.page@gmail.com>
2020-06-14 15:05:43 -0300

    created a2

---
+++ Assignments/A2/part1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+def LRGradDesc(data, target, wight_init, bias_init, learning_rate, max_iter):
+    print(CHII)
+
+if __name__ == "__main__":
+    LRGradDesc(0,0,0,0,0,0)
