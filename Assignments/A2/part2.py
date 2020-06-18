2e2a7cf5a9bb42831999e028058af496c67ab401
Ian Page <ian.am.page@gmail.com>
2020-06-17 21:31:13 -0300

    cleaned part2

--- Assignments/A2/part2.py
+++ Assignments/A2/part2.py
@@ -6,29 +6,18 @@ import pandas as pd
 from tensorflow.keras.datasets import reuters
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras import backend as K
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-#may not need
-from sklearn.preprocessing import LabelEncoder
-from sklearn.pipeline import Pipeline
 import matplotlib.pyplot as plt
 
 def graphData(X, y, xlabel):
 
-    max_loss = max([item[0] for item in y])
-    max_loss_val = max([item[1] for item in y])
-
     plt.clf()
-    plt.plot(X, [item[0] / max_loss for item in y], 'r', label='Training Loss')
-    plt.plot(X, [item[1] / max_loss_val for item in y], 'b', label='Validation Loss')
-    plt.plot(X, [item[2] for item in y], 'g', label='Training Acc')
-    plt.plot(X, [item[3] for item in y], 'y', label='Validation Acc')
-    plt.title('Training Loss vs Validation Loss')
+    plt.plot(X, [item[0] for item in y], 'b', label='Training Acc')
+    plt.plot(X, [item[1] for item in y], 'r', label='Validation Acc')
+    plt.title('Training Acc vs Validation Acc')
     plt.xlabel(xlabel)
-    plt.ylabel('Loss / Acc')
+    plt.ylabel('Acc')
     plt.legend()
     plt.show()  
 
@@ -51,12 +40,10 @@ def trainModel(X_train, y_train, X_val, y_val, hidden_layers = 3, hidden_nodes =
     K.set_value(model.optimizer.learning_rate, learning_rate)
     history = model.fit(X_train, y_train, epochs=epochs, batch_size=512, validation_data=(X_val, y_val), verbose = 0)
 
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
     acc = history.history['acc']
     val_acc = history.history['val_acc']
 
-    return loss[-1], val_loss[-1], acc[-1], val_acc[-1]
+    return acc[-1], val_acc[-1]
 
 if "__main__" == __name__:
 
@@ -85,26 +72,26 @@ if "__main__" == __name__:
     print("Testing hidden layer count")
     hidden_layer_data = []
     for hidden_layers in hidden_layer_set:
-        loss, loss_val, acc, acc_val = trainModel(partial_X_train, partial_y_train, X_val, y_val, hidden_layers=hidden_layers)
-        hidden_layer_data.append([loss, loss_val, acc, acc_val])
+        acc, acc_val = trainModel(partial_X_train, partial_y_train, X_val, y_val, hidden_layers=hidden_layers)
+        hidden_layer_data.append([acc, acc_val])
 
     print("Testing hidden layer node count")
     hidden_nodes_data = []
     for hidden_layer_nodes in hidden_nodes_set:
-        loss, loss_val, acc, acc_val = trainModel(partial_X_train, partial_y_train, X_val, y_val, hidden_nodes=hidden_layer_nodes)
-        hidden_nodes_data.append([loss, loss_val, acc, acc_val])
+        acc, acc_val = trainModel(partial_X_train, partial_y_train, X_val, y_val, hidden_nodes=hidden_layer_nodes)
+        hidden_nodes_data.append([acc, acc_val])
 
     print("Testing learning")
     learning_rate_data = []
     for learning_rate in learning_rate_set:
-        loss, loss_val, acc, acc_val = trainModel(partial_X_train, partial_y_train, X_val, y_val, learning_rate=learning_rate)
-        learning_rate_data.append([loss, loss_val, acc, acc_val])   
+        cc, acc_val = trainModel(partial_X_train, partial_y_train, X_val, y_val, learning_rate=learning_rate)
+        learning_rate_data.append([acc, acc_val])   
 
     print("Testing max epoch")
     epochs_data =[]
     for num_epochs in epochs_set:
-        loss, loss_val, acc, acc_val = trainModel(partial_X_train, partial_y_train, X_val, y_val, epochs=num_epochs)
-        epochs_data.append([loss, loss_val, acc, acc_val])
+        acc, acc_val = trainModel(partial_X_train, partial_y_train, X_val, y_val, epochs=num_epochs)
+        epochs_data.append([acc, acc_val])
 
     graphData(hidden_layer_set, hidden_layer_data, '# hidden layers')
     graphData(hidden_nodes_set, hidden_nodes_data, '# nodes in hidden layers')
