732b577f209291ae098531d3b21851e850580dad
Ian Page <ian.am.page@gmail.com>
2020-06-17 22:25:42 -0300

    got part2b working

--- Assignments/A2/part2b.py
+++ Assignments/A2/part2b.py
@@ -10,17 +10,6 @@ from tensorflow.keras.utils import to_categorical
 from tensorflow.keras import backend as K
 import matplotlib.pyplot as plt
 
-def graphData(y, xlabel):
-
-    plt.clf()
-    plt.plot(range(1, 21), [item[0] for item in y][0], 'b', label='Training Loss')
-    plt.plot(range(1, 21), [item[1] for item in y][0], 'r', label='Validation Loss')
-    plt.title('Training Loss vs Validation Loss')
-    plt.xlabel(xlabel)
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.show()  
-
 def vectorize_sequences(sequences, dimension=10000):
     results = np.zeros((len(sequences), dimension))
     for i,sequence in enumerate(sequences):
@@ -40,10 +29,9 @@ def trainModel(X_train, y_train, X_val, y_val, hidden_layers = 3, hidden_nodes =
     K.set_value(model.optimizer.learning_rate, learning_rate)
     history = model.fit(X_train, y_train, epochs=epochs, batch_size=512, validation_data=(X_val, y_val), verbose = 0)
 
-    loss = history.history['loss']
     val_loss = history.history['val_loss']
 
-    return loss, val_loss
+    return val_loss
 
 if "__main__" == __name__:
 
@@ -64,8 +52,19 @@ if "__main__" == __name__:
     y_val = one_hot_y_train[:1000]
     partial_y_train = one_hot_y_train[1000:]
 
-    epochs_data =[]
-    loss, loss_val = trainModel(partial_X_train, partial_y_train, X_val, y_val, epochs=20)
-    epochs_data.append([loss, loss_val])
+    batch_sizes = [200, 400, 600, 800, 1000]
+    color = ['b', 'r', 'y', 'g', 'o']
 
-    graphData(epochs_data, 'epochs')
+    i = 0
+    for batch_size in batch_sizes:
+
+        loss_val = trainModel(partial_X_train, partial_y_train, X_val, y_val, 1, 100, 0.001, 40)
+        label = 'Batch Size = ' + str(batch_size)
+        plt.plot(range(1, 21), loss_val, color[i], label=label)
+        i += 1
+
+    plt.title('Training Loss vs Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend() 
+    plt.show() 
