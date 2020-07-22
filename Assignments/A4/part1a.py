a0788d93260195d84a95c4c5d7ae61528a198513
Ian Page <ian.am.page@gmail.com>
2020-07-22 17:52:30 -0300

    got part1a finalised

--- Assignments/A4/part1a.py
+++ Assignments/A4/part1a.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd 
 from sklearn import datasets
-
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_samples, silhouette_score
@@ -12,113 +11,38 @@ from sklearn.metrics.cluster import adjusted_rand_score
 
 k_min = 2
 k_max = 6
-clrs = ['red', 'green', 'blue', 'yellow', 'purple']
+clrs = ['red', 'green', 'blue', 'yellow']
+clr_i = 0
+inits = ['kmeans++', 'random']
 silhouette_avgs = []
 adjusted_rand = []
-for k in range(k_min, k_max):
-    
-    # Create a subplot with 1 row and 2 columns
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    fig.set_size_inches(18, 7)
-
-    # The 1st subplot is the silhouette plot
-    # The silhouette coefficient can range from -1, 1 but in this example all
-    # lie within [-0.1, 1]
-    ax1.set_xlim([-0.1, 1])
-    # The (n_clusters+1)*10 is for inserting blank space between silhouette
-    # plots of individual clusters, to demarcate them clearly.
-    ax1.set_ylim([0, len(X) + (k + 1) * 10])
-    
-    km = KMeans(
-        n_clusters=k, init='random',
-        n_init=10, max_iter=300, 
-        tol=1e-04, random_state=0
-    )
-
-    y_km = km.fit_predict(X)
-
-    # computing sillouette avg score
-    silhouette_avg = silhouette_score(X, y_km)
-    silhouette_avgs.append(silhouette_avg)
-
-    adjusted_rand.append(adjusted_rand_score(y, y_km))
-
-     # Compute the silhouette scores for each sample
-    sample_silhouette_values = silhouette_samples(X, y_km)
-
-    y_lower = 10
-    for i in range(0, k):
-        # Aggregate the silhouette scores for samples belonging to
-        # cluster i, and sort them
-        ith_cluster_silhouette_values = \
-            sample_silhouette_values[y_km == i]
-
-        ith_cluster_silhouette_values.sort()
-
-        size_cluster_i = ith_cluster_silhouette_values.shape[0]
-        y_upper = y_lower + size_cluster_i
-
-        color = cm.nipy_spectral(float(i) / k)
-        ax1.fill_betweenx(np.arange(y_lower, y_upper),
-                          0, ith_cluster_silhouette_values,
-                          facecolor=color, edgecolor=color, alpha=0.7)
-
-        # Label the silhouette plots with their cluster numbers at the middle
-        ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
-
-        # Compute the new y_lower for next plot
-        y_lower = y_upper + 10  # 10 for the 0 samples
+for i in inits:
+    for k in range(k_min, k_max):
         
+        km = KMeans(
+            n_clusters=k, init='random'
+        )
 
-    ax1.set_title("The silhouette plot for the various clusters.")
-    ax1.set_xlabel("The silhouette coefficient values")
-    ax1.set_ylabel("Cluster label")
+        y_km = km.fit_predict(X)
 
-    # The vertical line for average silhouette score of all the values
-    ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+        # computing sillouette avg score
+        silhouette_avg = silhouette_score(X, y_km)
+        silhouette_avgs.append(silhouette_avg)
+        adjusted_rand.append(adjusted_rand_score(y, y_km))
 
-    ax1.set_yticks([])  # Clear the yaxis labels / ticks
-    ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
-
-    # 2nd Plot showing the actual clusters formed
-    colors = cm.nipy_spectral(y_km.astype(float) / k)
-    ax2.scatter(X[:, 0], X[:, 1], marker='.', s=30, lw=0, alpha=0.7,
-                c=colors, edgecolor='k')
-
-    # Labeling the clusters
-    centers = km.cluster_centers_
-    # Draw white circles at cluster centers
-    ax2.scatter(centers[:, 0], centers[:, 1], marker='o',
-                c="white", alpha=1, s=200, edgecolor='k')
-
-    for i, c in enumerate(centers):
-        ax2.scatter(c[0], c[1], marker='$%d$' % i, alpha=1,
-                    s=50, edgecolor='k')
-
-    ax2.set_title("The visualization of the clustered data.")
-    ax2.set_xlabel("Feature space for the 1st feature")
-    ax2.set_ylabel("Feature space for the 2nd feature")
-
-    plt.suptitle(("Silhouette analysis for KMeans clustering on sample data "
-                  "with n_clusters = %d" % k),
-                 fontsize=14, fontweight='bold')
+    sil_title = i + ": silhouette score"
+    adj_title = i + ": adjust rand index"
+    sil_clr = clrs[clr_i]
+    adj_clr = clrs[clr_i + 1]
+    plt.plot(range(k_min, k_max), silhouette_avgs, c=sil_clr, label='silhouette score')
+    plt.plot(range(k_min, k_max), adjusted_rand, c=adj_clr, label='adjust rand index')
+    silhouette_avgs = []
+    adjusted_rand = []
+    clr_i += 2
 
 
-    plt.show()
-
-plt.clf()
-plt.plot(range(k_min, k_max), silhouette_avgs, c='red', label='silhouette score')
-plt.plot(range(k_min, k_max), adjusted_rand, c='blue', label='adjust rand index')
 plt.legend()
-plt.xlabel('n clusters')
+plt.xlabel('K')
 plt.ylabel('Score Metric')
+plt.suptitle("Scoring Metrics for KMeans Over K")
 plt.show()
-
-for k in range(0, k_max - k_min):
-    print("For n_clusters =", k + k_min,
-        "The average silhouette_score is :", silhouette_avgs[k])
-
-print("\n")
-for k in range(0, k_max - k_min):
-    print("For n_clusters =", k + k_min,
-        "The adjusted random score is :", adjusted_rand[k])
